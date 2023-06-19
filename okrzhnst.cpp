9e0dad095153a81a9d01d3a06c543899b983502a
Anastasiia Nikitina <@Anastasiia-Nikitina>
2023-06-19 22:26:24 +0300

    + .gitignore

---
+++ okrzhnst.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, y, r, d;
+	setlocale (LC_ALL, "Russian");
+	cout << "Введите через пробел координаты точки"<<endl;
+	cin >> x >> y ;
+	cout << "Введите радиус окружности"<<endl;
+	cin >> r;
+	d = pow((x*x + y*y) , 0.5);
+		if (d <= r)
+	 {
+	 	cout << "Данная точка лежит внутри окружности";
+	 }
+	 else 
+	 {
+	 	cout << "Данная точка лежит за пределами окружности";
+	 }
+	return 0;
+}
